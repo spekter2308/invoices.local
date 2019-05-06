ca11725bcd293d583ee2faa98a168c9d3c9f1c9b
Aliosha <aliosha.shalabay@gmail.com>
2019-05-06 17:25:11 +0300

    Item select

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 //invoice part
 Route::get('invoices', 'InvoiceController@index');
 Route::get('invoices/create', 'InvoiceController@create');
+Route::get('invoices/select-item', 'InvoiceController@selectItem')->name('select-item');
+Route::get('invoices/get/select-item', 'InvoiceController@getSelectItem')->name('get-select-item');
+Route::get('invoices/create/select-item/{id?}', 'InvoiceController@createSelectItem')->name('create-select-item')->where(['id' => '[0-9]+']);
+Route::post('invoices/save/select-item/{id?}', 'InvoiceController@saveSelectItem')->name('save-select-item')->where(['id' => '[0-9]+']);
+Route::delete('invoices/delete/select-item/{id}', 'InvoiceController@deleteSelectItem')->name('delete-select-item')->where(['id' => '[0-9]+']);
+
 Route::post('invoices', 'InvoiceController@store');
 
 //customers part
@@ -36,6 +42,6 @@ Route::delete('customers/{customer}', 'CustomerController@destroy');
 //companies part
 Route::get('company', 'CompanyController@index')->name('company-list');
 Route::get('company/create', 'CompanyController@create')->name('company-create');
-Route::get('company/update/{id}', 'CompanyController@update')->name('company-update');
+Route::get('company/update/{id}', 'CompanyController@update')->name('company-update')->where(['id' => '[0-9]+']);
 Route::post('company/create/save', 'CompanyController@createSave')->name('company-create-save');
-Route::post('company/upload/save/{id}', 'CompanyController@updateSave')->name('company-upload-save');
+Route::post('company/upload/save/{id}', 'CompanyController@updateSave')->name('company-upload-save')->where(['id' => '[0-9]+']);
