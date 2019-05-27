ddb09a0dbda69d82b7ffc469f6a55c8c7b35d11b
Plakhotniuk <plaxotpos@gmail.com>
2019-05-27 16:56:50 +0300

    pdf template

--- resources/views/pdf/invoices.blade.php
+++ resources/views/pdf/invoices.blade.php
@@ -2,9 +2,47 @@
     .invoice-pdf {
     
     }
+    
 </style>
 <div class="invoice-pdf">
 
+    <table>
+        <tr>
+            <td>company</td>
+            <td>logo</td>
+        </tr>
+        <tr>
+            <td>customer</td>
+            <td>date</td>
+        </tr>
+    </table>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>item</th>
+                <th>description</th>
+                <th>unit price</th>
+                <th>quantity</th>
+                <th>tax</th>
+                <th>amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>item_body</td>
+                <td>description_body</td>
+                <td>unit_price_body</td>
+                <td>quantity_body</td>
+                <td>tax_body</td>
+                <td>amount_body</td>
+            </tr>
+    
+            <tr>
+                <td>notes</td>
+            </tr>
+        </tbody>
+    </table>
 
 </div>
 <script>
