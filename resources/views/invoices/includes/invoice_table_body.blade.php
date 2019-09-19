632249e0445533414614bf98c6349482806a0501
Olexandr Plakhotniut <plaxotpos@gmail.com>
2019-09-19 20:10:45 +0300

    duplicate button check work

--- resources/views/invoices/includes/invoice_table_body.blade.php
+++ resources/views/invoices/includes/invoice_table_body.blade.php
@@ -82,7 +82,7 @@
                 @endif>
             {{ $invoice->status }}
         </td>
-        <td>
+        <td style="display: flex; flex-direction: row; justify-content: space-around;">
             @if ($invoice->status != "Paid")
                 <a href="{{route('mark-as-paid', ['id' => $invoice->id])}}" class="btn btn-sm btn-success">
                     Mark Paid
@@ -92,6 +92,9 @@
                     Mark Unpaid
                 </a>
             @endif
+            <a href="invoices/duplicate/{{$invoice->id}}" class="btn btn-sm btn-primary">
+                Duplicate
+            </a>
         </td>
     </tr>
 @empty
